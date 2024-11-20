a0f8d01c486b2e68da277d7bf431f1baa5e0f4ce
Rasmus Tornby Arendt <112378651+RasmanHuhu@users.noreply.github.com>
2024-11-20 10:55:34 +0100

    Visual identity
    Setup some visual identity, background picture aswell as buttons to test how it'll look.

--- Frontend/src/pages/TemplateSuggestion.jsx
+++ Frontend/src/pages/TemplateSuggestion.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Importér Link fra React Router
+import { Link } from "react-router-dom";
 import maleDev from "../assets/maleDev.png";
-import "../assets/Frontpage.css";
+import "../assets/TemplateSuggestion.css";
+import ButtonRow from "../components/ButtonRow";
 
 const TemplateSuggestion = () => {
-return (
-    <div className="frontpage" style={{ backgroundImage: `url(${maleDev})` }}>
-      <div className="content-box">
-        <h1 className="main-header">Let's get you started
-         </h1>
+    return (
+        <div className="frontpage" style={{ backgroundImage: `url(${maleDev})` }}>
+            <div className="content-box">
+                <h2 className="sub-header"></h2>
 
-        
-          <h2 className="sub-header">Here's a few suggestions for your own personalized portfolio</h2>
-        
+                <div>
+                <h2 className="sub-header">Choose a template to proceed</h2>
+                    <ButtonRow />                    
+                </div>
 
-    </div>
-    </div>
-);
+                <h4>More templates coming soon!</h4>
+
+                <Link to="/editor" className="styled-button">Test</Link>
+
+
+
+            </div>
+        </div>
+    );
 };
 
 export default TemplateSuggestion;
