19d46fc8379760c0bedc6f40080c51811858c702
Kussemar <marcus.dofinsmurt@gmail.com>
2024-11-22 12:38:53 +0100

    Har du allerede en bruger, klik her

--- Frontend/src/pages/Frontpage.jsx
+++ Frontend/src/pages/Frontpage.jsx
@@ -8,26 +8,31 @@ const Frontpage = () => {
   return (
     <div className="frontpage" style={{ backgroundImage: `url(${dameImage})` }}>
       <div className="content-box">
-        <h1 className="main-header">Lav nemt din egen professionelle hjemmeside</h1>
+
+        <h1 className="main-header">Lav nemt <br></br> din egen professionelle<br></br> hjemmeside</h1>
         
         <div className="section">
           <h2 className="sub-header">Nemt træk og placer overalt!</h2>
-          <p>Tilpas din hjemmeside med lige præcis hvad DU vil!</p>
+          <p>Tilpas din hjemmeside med lige præcis<br></br> hvad DU vil!</p>
         </div>
         
         <div className="section">
           <h2 className="sub-header">Dit eget domæne</h2>
-          <p>Du får dit helt eget personlige domæne, så din hjemmeside tilhører 100% dig!</p>
+          <p>Du får dit helt eget personlige domæne<br></br>så din hjemmeside tilhører 100% dig!</p>
         </div>
         
         <div className="section">
           <h2 className="sub-header">Sikker hosting</h2>
-          <p>Som kunde hos os er du garanteret at din personlige hjemmeside altid er tilgængelig!</p>
+          <p>Som kunde hos os er du garanteret<br></br>at din personlige hjemmeside<br></br>altid er tilgængelig!</p>
         </div>
         
         <Link to="/editor" className="frontpage-button">
           <span>Kom i gang!</span>
         </Link>
+
+        <div className="login-prompt">
+          <p>Har du allerede en bruger? Klik <Link to="/login" className="login-link">her</Link></p>
+        </div>
       </div>
     </div>
   );
