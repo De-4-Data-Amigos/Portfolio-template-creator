f5f4e5c85c7b16434fc6665d15185dc0cb80bc16
Kussemar <marcus.dofinsmurt@gmail.com>
2024-12-12 14:35:57 +0100

    Added text component from rasmus to toolbar

--- Frontend/src/components/Toolbar.jsx
+++ Frontend/src/components/Toolbar.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import "../assets/Toolbar.css";
 import ChooseBackground from './ChooseBackground';
+import EditableTextInputField from './EditableTextInputField';
 
 const Toolbar = ({addComponent, children }) => {
   const [showChooseBackground, setShowChooseBackground] = useState(false);
 
   function addText() {
-    addComponent(<div className="text-component">New text</div>);
+    
+    addComponent(<EditableTextInputField text="Test input if save"/>);
   }
 
   return (
